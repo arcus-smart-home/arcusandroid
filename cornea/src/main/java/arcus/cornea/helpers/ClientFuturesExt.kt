a9d92181761ed707a01b71a3af8a44de78399238
Erik Anderson <erik.j.anderson@gmail.com>
2020-01-03 13:22:36 -0600

    Add coroutine support to Cornea Expose a future -> coroutine helper method Add coroutine support to presentation module

--- cornea/src/main/java/arcus/cornea/helpers/ClientFuturesExt.kt
+++ cornea/src/main/java/arcus/cornea/helpers/ClientFuturesExt.kt
@@ -17,9 +17,15 @@
 package arcus.cornea.helpers
 
 import arcus.cornea.utils.Listeners
+import com.google.common.util.concurrent.Uninterruptibles
 import com.iris.client.ClientEvent
 import com.iris.client.event.ClientFuture
 import com.iris.client.event.Futures
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.concurrent.ExecutionException
+import kotlin.coroutines.resumeWithException
 
 /**
  * Tries to run the [action] if the original requests response was not null.
@@ -55,4 +61,49 @@ inline fun <reified T, reified U> ClientFuture<T?>.transformNonNull(crossinline 
     it?.let {
         action(it)
     } ?: throw RuntimeException("Error value was null - expected non-null.")
+}
+
+/**
+ * Awaits completion of `this` [ClientFuture] without blocking a thread.
+ *
+ * This suspend function is cancellable.
+ *
+ * If the [Job] of the current coroutine is cancelled or completed while this suspending function is waiting, this
+ * function stops waiting for the future and immediately resumes with
+ * [CancellationException][kotlinx.coroutines.CancellationException].
+ *
+ * This method is intended to be used with one-shot Futures, so on coroutine cancellation, the Future is cancelled as
+ * well. If cancelling the given future is undesired, use [kotlinx.coroutines.NonCancellable].
+ *
+ * Variation of the ListenableFuture version from: https://github.com/Kotlin/kotlinx.coroutines/blob/master/integration/kotlinx-coroutines-guava/src/ListenableFuture.kt
+ * Copyright 2016-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+suspend fun <T> ClientFuture<T>.await(): T {
+    try {
+        if (isDone) return Uninterruptibles.getUninterruptibly(this)
+    } catch (e: ExecutionException) {
+        // ExecutionException is the only kind of exception that can be thrown from a gotten
+        // Future, other than CancellationException. Cancellation is propagated upward so that
+        // the coroutine running this suspend function may process it.
+        // Any other Exception showing up here indicates a very fundamental bug in a
+        // Future implementation.
+        throw e.cause!!
+    }
+
+    return suspendCancellableCoroutine { cont: CancellableContinuation<T> ->
+        onCompletion {
+            if (isCancelled) {
+                cont.cancel()
+            } else {
+                try {
+                    cont.resumeWith(Result.success(Uninterruptibles.getUninterruptibly(this)))
+                } catch (e: ExecutionException) {
+                    cont.resumeWithException(e.cause!!)
+                }
+            }
+        }
+        cont.invokeOnCancellation {
+            cancel(false)
+        }
+    }
 }
