92fb17d75c5fcb538230060fed70a0060f3fc871
Erik Anderson <erik.j.anderson@gmail.com>
2020-01-04 12:21:49 -0600

    Update SettingsUpdatePin look and feel... Left this as Java since this needs way more love and attention and really just tried to get look and feel updated...

--- app/src/main/java/arcus/app/account/settings/SettingsUpdatePin.java
+++ app/src/main/java/arcus/app/account/settings/SettingsUpdatePin.java
@@ -15,40 +15,35 @@
  */
 package arcus.app.account.settings;
 
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.google.common.base.Strings;
-import arcus.cornea.controller.PersonController;
 import com.iris.client.exception.ErrorResponseException;
 import com.iris.client.model.PersonModel;
-import arcus.app.R;
-import arcus.app.common.backstack.BackstackManager;
-import arcus.app.common.error.ErrorManager;
-import arcus.app.common.error.type.OtherErrorTypes;
-import arcus.app.common.popups.YesNoPopupColored;
-import arcus.app.common.sequence.SequencedFragment;
-import arcus.app.subsystems.alarm.AlertFloatingFragment;
-import arcus.app.subsystems.people.controller.NewPersonSequenceController;
-import arcus.app.subsystems.people.model.PersonTypeSequence;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SettingsUpdatePin extends SequencedFragment implements View.OnClickListener, PersonController.Callback, YesNoPopupColored.Callback {
+import arcus.app.R;
+import arcus.app.common.error.ErrorManager;
+import arcus.app.common.fragments.ModalErrorBottomSheet;
+import arcus.app.common.fragments.ModalErrorBottomSheetSingleButton;
+import arcus.app.common.sequence.SequencedFragment;
+import arcus.app.subsystems.people.controller.NewPersonSequenceController;
+import arcus.app.subsystems.people.model.PersonTypeSequence;
+import arcus.cornea.controller.PersonController;
+import kotlin.Unit;
+
+public class SettingsUpdatePin extends SequencedFragment implements View.OnClickListener, PersonController.Callback {
 
     private final static String SCREEN_VARIANT = "SCREEN_VARIANT";
     private final static String PERSON_ID = "PERSON_ID";
@@ -59,39 +54,41 @@ public class SettingsUpdatePin extends SequencedFragment implements View.OnClick
     @NonNull
     private String[] pinNumbers = new String[2];
 
-    private final int[] circles = {
-        R.id.pin_number_1_circle,
-        R.id.pin_number_2_circle,
-        R.id.pin_number_3_circle,
-        R.id.pin_number_4_circle
-    };
-
-    private final int[] numbers = {
-        R.id.pin_number_pad_0,
-        R.id.pin_number_pad_1,
-        R.id.pin_number_pad_2,
-        R.id.pin_number_pad_3,
-        R.id.pin_number_pad_4,
-        R.id.pin_number_pad_5,
-        R.id.pin_number_pad_6,
-        R.id.pin_number_pad_7,
-        R.id.pin_number_pad_8,
-        R.id.pin_number_pad_9
-    };
-
     @NonNull
     private List<ImageView> circleViews = new ArrayList<>();
     private TextView stepInstructions;
-    private Drawable filledCircle;
-    private Drawable hollowCircle;
+
+    private final int[] settingsStrings = new int[] {
+            R.string.place_creation_pin_code_title,
+            R.string.people_confirm_pin
+    };
+
+    private final int[] addAPersonStrings = new int[] {
+            R.string.people_create_pin,
+            R.string.people_confirm_pin
+    };
+
+    private final int[] accountCreationStrings = new int[] {
+            R.string.Account_registration_enter_pin_code_message,
+            R.string.Account_registration_confirm_pin_code_message
+    };
+
+    private final int[] addAPlaceCreationStrings = new int[] {
+            R.string.place_creation_pin_code_title,
+            R.string.people_confirm_pin
+    };
+
+    @DrawableRes
+    private int filledCircle;
+
+    @DrawableRes
+    private int hollowCircle;
 
     @NonNull
     private ScreenVariant variant = ScreenVariant.SETTINGS;
     @Nullable
     private String personId;
     private String placeID;
-
-    SettingsUpdatePin fragment;
 
     public enum ScreenVariant {
         SETTINGS,
@@ -132,72 +129,46 @@ public class SettingsUpdatePin extends SequencedFragment implements View.OnClick
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = super.onCreateView(inflater, container, savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         //hide the softkeyboard. In the rare event of exception, simply log the exception and continue.
         try {
-            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+            requireActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         } catch (Exception e) {
             // Log
         }
 
-        int openCircleResId = isLightColorScheme() ? R.drawable.sidemenu_settings_whitecircle : R.drawable.sidemenu_settings_blackcircle;
-        int closedCircleResId = isLightColorScheme() ? R.drawable.sidemenu_settings_whitecircle_filled : R.drawable.sidemenu_settings_blackcircle_filled;
-        int deleteResId = isLightColorScheme() ? R.drawable.sidemenu_settings_delete : R.drawable.sidemenu_settings_delete_black;
-
-        for (int i = 0; i < numbers.length; i++) {
-            TextView current = (TextView) view.findViewById(numbers[i]);
-            current.setText(String.format("%s", i));
-            current.setOnClickListener(this);
+        String resourceType = "id";
+        String packageName = view.getContext().getPackageName();
+        for (int index = 0; index < 10; index++) {
+            String idName = "pin_number_pad_" + index;
+            int id = getResources().getIdentifier(idName, resourceType, packageName);
+            view.findViewById(id).setOnClickListener(this);
         }
+        view.findViewById(R.id.pin_number_pad_backspace).setOnClickListener(this);
 
-        stepInstructions = (TextView) view.findViewById(R.id.pin_pad_header_text);
+        stepInstructions = view.findViewById(R.id.pin_pad_header_text);
         stepInstructions.setText(getTitleString());
-        stepInstructions.setTextColor(isLightColorScheme() ? Color.WHITE : Color.BLACK);
 
-        ImageView deleteButton = (ImageView) view.findViewById(R.id.pin_number_pad_backspace);
-        deleteButton.setOnClickListener(this);
-        deleteButton.setImageResource(deleteResId);
-
-        for (int thisNumberResId : numbers) {
-            TextView thisNumber = (TextView) view.findViewById(thisNumberResId);
-            thisNumber.setBackgroundResource(openCircleResId);
-            thisNumber.setTextColor(isLightColorScheme() ? Color.WHITE : Color.BLACK);
+        for (int index = 1; index < 5; index++) {
+            String idName = "pin_number_" + index + "_circle";
+            int id = getResources().getIdentifier(idName, resourceType, packageName);
+            circleViews.add(view.findViewById(id));
         }
 
-        for (int i = 0; i < circles.length; i++) {
-            ImageView thisCircle = (ImageView) view.findViewById(circles[i]);
-            thisCircle.setImageResource(openCircleResId);
-            circleViews.add(thisCircle);
-        }
-
-        filledCircle = ContextCompat.getDrawable(getActivity(), closedCircleResId);
-        hollowCircle = ContextCompat.getDrawable(getActivity(), openCircleResId);
+        filledCircle = R.drawable.sidemenu_settings_blackcircle_filled;
+        hollowCircle = R.drawable.sidemenu_settings_blackcircle;
 
         PersonController.instance().edit(personId, this);
-
-        return view;
     }
 
     public void onClick(@NonNull View view) {
-        switch (view.getId()) {
-            case R.id.pin_number_pad_0:
-            case R.id.pin_number_pad_1:
-            case R.id.pin_number_pad_2:
-            case R.id.pin_number_pad_3:
-            case R.id.pin_number_pad_4:
-            case R.id.pin_number_pad_5:
-            case R.id.pin_number_pad_6:
-            case R.id.pin_number_pad_7:
-            case R.id.pin_number_pad_8:
-            case R.id.pin_number_pad_9:
-                String pinNumber = ((TextView)view).getText().toString();
-                appendChar(pinNumber);
-                break;
-            case R.id.pin_number_pad_backspace:
-                removeLastChar();
-                break;
+        if (view.getId() == R.id.pin_number_pad_backspace) {
+            removeLastChar();
+        } else {
+            String pinNumber = ((TextView) view).getText().toString();
+            appendChar(pinNumber);
         }
     }
 
@@ -208,18 +179,16 @@ public class SettingsUpdatePin extends SequencedFragment implements View.OnClick
 
         if (Strings.isNullOrEmpty(pinNumbers[workingOn])) {
             pinNumbers[workingOn] = character;
-            circleViews.get(0).setImageDrawable(filledCircle);
-        }
-        else if (pinNumbers[workingOn].length() != 4) {
+            circleViews.get(0).setImageResource(filledCircle);
+        } else if (pinNumbers[workingOn].length() != 4) {
             pinNumbers[workingOn] += character;
-            circleViews.get(pinNumbers[workingOn].length() - 1).setImageDrawable(filledCircle);
+            circleViews.get(pinNumbers[workingOn].length() - 1).setImageResource(filledCircle);
         }
 
         if (pinNumbers[workingOn].length() == 4) {
             if (workingOn == 1) {
                 processUpdate();
-            }
-            else {
+            } else {
                 pinNumbers[++workingOn] = "";
                 stepInstructions.setText(getTitleString());
                 hollowDots();
@@ -232,19 +201,23 @@ public class SettingsUpdatePin extends SequencedFragment implements View.OnClick
             int length = pinNumbers[workingOn].length();
 
             pinNumbers[workingOn] = pinNumbers[workingOn].substring(0, length - 1);
-            circleViews.get(length - 1).setImageDrawable(hollowCircle);
+            circleViews.get(length - 1).setImageResource(hollowCircle);
         }
     }
 
     private void hollowDots() {
         for (int i = 0; i < circleViews.size(); i++) {
-            circleViews.get(i).setImageDrawable(hollowCircle);
+            circleViews.get(i).setImageResource(hollowCircle);
         }
     }
 
-    public void processUpdate() {
+    private void processUpdate() {
         if (!pinNumbers[0].equals(pinNumbers[1])) {
-            ErrorManager.in(getActivity()).show(OtherErrorTypes.PIN_NUMBER_MISMATCH);
+            ModalErrorBottomSheetSingleButton.newInstance(
+                    getString(R.string.pin_code_not_match_title),
+                    getString(R.string.pin_code_not_match_text),
+                    getString(R.string.dismiss)
+            ).show(getFragmentManager());
             resetForm();
             return;
         }
@@ -260,33 +233,7 @@ public class SettingsUpdatePin extends SequencedFragment implements View.OnClick
         stepInstructions.setText(getTitleString());
     }
 
-    private boolean isLightColorScheme () {
-        return variant == ScreenVariant.SETTINGS;
-    }
-
     private String getTitleString () {
-
-        int settingsStrings[] = new int[] {
-                R.string.place_creation_pin_code_title,
-                R.string.people_confirm_pin
-        };
-
-        int addAPersonStrings[] = new int[] {
-                R.string.people_create_pin,
-                R.string.people_confirm_pin
-        };
-
-        int accountCreationStrings[] = new int[] {
-                R.string.Account_registration_enter_pin_code_message,
-                R.string.Account_registration_confirm_pin_code_message
-        };
-
-        int addAPlaceCreationStrings[] = new int[] {
-                R.string.place_creation_pin_code_title,
-                R.string.people_confirm_pin
-        };
-
-
         switch (variant) {
             case SETTINGS: return getString(settingsStrings[workingOn]);
             case ADD_A_PERSON: return getString(addAPersonStrings[workingOn]);
@@ -297,12 +244,6 @@ public class SettingsUpdatePin extends SequencedFragment implements View.OnClick
         }
     }
 
-    public void onResume() {
-        super.onResume();
-        fragment = this;
-        getActivity().setTitle(getTitle().toUpperCase());
-    }
-
     @Override
     public String getTitle() {
         switch (variant) {
@@ -310,9 +251,9 @@ public class SettingsUpdatePin extends SequencedFragment implements View.OnClick
             case ADD_A_PERSON:
             case ADD_A_PLACE: return getString(R.string.people_pin_code);
 
-            case SETTINGS: return getString(R.string.change_pincode); // Editing Existing Pin Code
+            case SETTINGS:
             default:
-                return getString(R.string.change_pincode);
+                return getString(R.string.change_pincode); // Editing Existing Pin Code
         }
     }
 
@@ -352,17 +293,12 @@ public class SettingsUpdatePin extends SequencedFragment implements View.OnClick
 
         ErrorResponseException errorResponseException = (ErrorResponseException) throwable;
         if (errorResponseException.getCode().equals("pin.notUniqueAtPlace")) {
-            final AlertFloatingFragment floatingFragment =
-                    AlertFloatingFragment.newInstance(
-                            getActivity().getString(R.string.pin_code_not_unique_title),
-                            getActivity().getString(R.string.pin_code_not_unique_text),
-                            null, null, null, null,
-                            new AlertFloatingFragment.AlertDismissedCallback() {
-                                @Override
-                                public void dismissed() { }
-                            });
-            BackstackManager.getInstance()
-                            .navigateToFloatingFragment (floatingFragment, floatingFragment.getTag(), true);
+            ModalErrorBottomSheetSingleButton bottomSheet = ModalErrorBottomSheetSingleButton.newInstance(
+                    getString(R.string.pin_code_not_unique_title), // title
+                    getString(R.string.pin_code_not_unique_text), // description
+                    getString(R.string.dismiss) // bottom
+            );
+            bottomSheet.show(getFragmentManager());
         } else {
             ErrorManager.in(getActivity()).showGenericBecauseOf(throwable);
         }
@@ -385,31 +321,22 @@ public class SettingsUpdatePin extends SequencedFragment implements View.OnClick
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         if (item.getItemId() == R.id.menu_skip) {
-            YesNoPopupColored popup = YesNoPopupColored.newInstance(getString(R.string.no_pin_code), getString(R.string.no_pin_code_description));
-            popup.setCallback(fragment);
-            BackstackManager.getInstance().navigateToFloatingFragment(popup, popup.getClass().getSimpleName(), true);
+            ModalErrorBottomSheet bottomSheet = ModalErrorBottomSheet.newInstance(
+                    getString(R.string.no_pin_code), // title
+                    getString(R.string.no_pin_code_description), // description
+                    getString(android.R.string.yes), // top
+                    getString(android.R.string.no) // bottom
+            );
+            bottomSheet.setGetSupportAction(() -> {
+                goNext();
+                return Unit.INSTANCE;
+            });
+            bottomSheet.show(getFragmentManager());
             return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void yes() {
-        BackstackManager.getInstance().navigateBack();
-        goNext();
-    }
-
-    @Override
-    public void no() {
-        BackstackManager.getInstance().navigateBack();
     }
 }
--- app/src/main/java/arcus/app/common/fragments/ModalErrorBottomSheet.kt
+++ app/src/main/java/arcus/app/common/fragments/ModalErrorBottomSheet.kt
@@ -21,15 +21,15 @@ import android.view.View
 import arcus.app.R
 import arcus.app.common.utils.ActivityUtils
 import android.widget.Button
-import arcus.app.common.view.ScleraTextView
-
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class ModalErrorBottomSheet : ModalBottomSheet() {
 
     private lateinit var errorTitle : String
     private lateinit var errorDescription : String
-    private lateinit var topButtonText : String
-    private lateinit var bottomButtonText : String
+    private var topButtonText : String? = null
+    private var bottomButtonText : String? = null
     private var actionListener : () -> Unit = {
         ActivityUtils.launchSupport()
     }
@@ -42,29 +42,39 @@ class ModalErrorBottomSheet : ModalBottomSheet() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            errorTitle = it.getString(ARG_TITLE)!!
-            errorDescription = it.getString(ARG_DESCRIPTION)!!
-            topButtonText = it.getString(ARG_TOP_BUTTON_TEXT)!!
-            bottomButtonText = it.getString(ARG_BOTTOM_BUTTON_TEXT)!!
+            errorTitle = it.getString(ARG_TITLE, "")
+            errorDescription = it.getString(ARG_DESCRIPTION, "")
+            topButtonText = it.getString(ARG_TOP_BUTTON_TEXT)
+            bottomButtonText = it.getString(ARG_BOTTOM_BUTTON_TEXT)
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val errorTitleTextView = view.findViewById<ScleraTextView>(R.id.modal_error_title)
+        val errorTitleTextView = view.findViewById<TextView>(R.id.modal_error_title)
         errorTitleTextView?.text = errorTitle
 
-        val errorDescriptionTextView = view.findViewById<ScleraTextView>(R.id.modal_error_description)
+        val errorDescriptionTextView = view.findViewById<TextView>(R.id.modal_error_description)
         errorDescriptionTextView?.text = errorDescription
 
-        view.findViewById<Button>(R.id.modal_error_support_button)?.text = topButtonText
-        view.findViewById<Button>(R.id.modal_error_dismiss_button)?.text = bottomButtonText
-        view.findViewById<Button>(R.id.modal_error_support_button)?.setOnClickListener {
-            actionListener.invoke()
-            dismiss()
+        val supportButton = view.findViewById<Button>(R.id.modal_error_support_button)
+        if (topButtonText == null) {
+            supportButton.isVisible = false
+        } else {
+            supportButton.text = topButtonText
+            supportButton.setOnClickListener {
+                actionListener.invoke()
+                dismiss()
+            }
         }
 
-        view.findViewById<Button>(R.id.modal_error_dismiss_button).setOnClickListener {
-            dismiss()
+        val dismissButton = view.findViewById<Button>(R.id.modal_error_dismiss_button)
+        if (bottomButtonText == null) {
+            dismissButton.isVisible = false
+        } else {
+            dismissButton.text = bottomButtonText
+            dismissButton.setOnClickListener {
+                dismiss()
+            }
         }
     }
 
@@ -86,21 +96,24 @@ class ModalErrorBottomSheet : ModalBottomSheet() {
     }
 
     companion object {
-        private val ARG_TITLE = "ARG_TITLE"
-        private val ARG_DESCRIPTION = "ARG_DESCRIPTION"
-        private val ARG_TOP_BUTTON_TEXT = "ARG_TOP_BUTTON_TEXT"
-        private val ARG_BOTTOM_BUTTON_TEXT = "ARG_BOTTOM_BUTTON_TEXT"
+        private const val ARG_TITLE = "ARG_TITLE"
+        private const val ARG_DESCRIPTION = "ARG_DESCRIPTION"
+        private const val ARG_TOP_BUTTON_TEXT = "ARG_TOP_BUTTON_TEXT"
+        private const val ARG_BOTTOM_BUTTON_TEXT = "ARG_BOTTOM_BUTTON_TEXT"
 
         @JvmStatic
-        fun newInstance(title : String, description :String, topButtonText: String, bottomButtonText: String) : ModalErrorBottomSheet {
-            val args = Bundle()
-            args.putString(ARG_TITLE, title)
-            args.putString(ARG_DESCRIPTION, description)
-            args.putString(ARG_TOP_BUTTON_TEXT, topButtonText)
-            args.putString(ARG_BOTTOM_BUTTON_TEXT, bottomButtonText)
-            val fragment = ModalErrorBottomSheet()
-            fragment.arguments = args
-            return fragment
+        fun newInstance(
+            title: String,
+            description: String,
+            topButtonText: String? = null,
+            bottomButtonText: String? = null
+        ) : ModalErrorBottomSheet = ModalErrorBottomSheet().apply {
+            arguments = Bundle().apply {
+                putString(ARG_TITLE, title)
+                putString(ARG_DESCRIPTION, description)
+                putString(ARG_TOP_BUTTON_TEXT, topButtonText)
+                putString(ARG_BOTTOM_BUTTON_TEXT, bottomButtonText)
+            }
         }
     }
 }
